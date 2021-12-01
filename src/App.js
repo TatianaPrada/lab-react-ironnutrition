5ce90986502d05d088f19706e2a4bf3b445d9803
TatianaPrada <tatiana.1227@hotmail.com>
2021-12-01 20:09:13 +0000

    interaction 4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import "./App.css";
 import FoodBox from "./components/FoodBox/FoodBox";
 import { Card, Row, Col, Divider, Input, Button } from "antd";
 import foods from "./foods.json";
+import { useState } from "react";
+import AddFoodForm from './components/AddFoodForm/AddFoodForm'
 
 
+ 
 function App() {
+
+  const[foodsArray, setFoodsArray] = useState(foods)
+
   return <div className="App">
+  <AddFoodForm foodsArray={foodsArray} setFoodsArray={setFoodsArray}/>
   <h2>Food List: </h2>
   <Col>
-    {foods.map((item, index) => {
+    {foodsArray.map((item, index) => {
       return (
       <FoodBox item={item} key={index + Date.now()}/>
       )
